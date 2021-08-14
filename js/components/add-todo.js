a0e8cc28822788f7da9c2cf5e6d39644937c41bf
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2021-08-14 19:35:42 -0300

    add checkbox function + removeTodo

--- js/components/add-todo.js
+++ js/components/add-todo.js
@@ -1,7 +1,18 @@
-class AddTodo {
+export default class AddTodo {
   constructor() {
     this.btn = document.getElementById('add');
     this.title = document.getElementById('title');
     this.description = document.getElementById('description');
   }
+
+  onClick(callback) {
+    this.btn.onclick = () => {
+      if (title.value === '' || description.value === ''){
+        console.error('Incorrecto');
+      } else {
+        callback(this.title.value, this.description.value);
+      }
+    }
+  }
 }
+
--- js/model.js
+++ js/model.js
@@ -13,6 +13,17 @@ export default class Model {
     return this.todos;
   }
 
+  findTodo(id) {
+    return this.todos.findIndex((todo) => todo.id === id);
+  }
+
+  toggleCompleted(id) {
+    const index = this.findTodo(id);
+    const todo = this.todos[index];
+    todo.completed = !todo.completed;
+    console.log(this.todos);
+  }
+
   addTodo(title, description) {
     const todo = {
       // si una propiedad es igual a su valor (title: title), podemos usar (title)
@@ -26,5 +37,10 @@ export default class Model {
 
     // return Object.assign({}, todo) = CLONAR OBJETO
     return {...todo};
+  }
+
+  removeTodo(id) {
+    const index = this.findTodo(id);
+    this.todos.splice(index, 1);
   }
 }
